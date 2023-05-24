50908758df2548bf3c6347f708cfe946de2fdb0d
Egor Loktev <loktev@lok-tech.com>
2023-05-24 22:26:27 +0300

    disable cors

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
