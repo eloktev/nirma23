7921d2a1efcaeada827da43932a9a284ae51ef9c
Egor Loktev <loktev@lok-tech.com>
2023-06-16 17:33:15 +0300

    Add file exporter

--- api/api_v1/endpoints/messages.py
+++ api/api_v1/endpoints/messages.py
@@ -7,8 +7,10 @@ from fastapi.exceptions import HTTPException
 from schemas.location import Location
 import dao, models, schemas
 from api import deps
-
+from fastapi.responses import Response, FileResponse, StreamingResponse
 from models.document import DocumentStatus
+import pandas, io
+
 
 router = APIRouter()
 
@@ -25,6 +27,67 @@ def get_document_messages(document_id: UUID,
         raise HTTPException(status_code=425, detail="Markup in progress for this document")
     return dao.dao_message.get_by_file_id(db, document_id=document_id)
 
+@router.get("/{document_id}/file", 
+            response_class=FileResponse
+            )
+def get_document_messages(document_id: UUID,
+                          db: Session = Depends(deps.get_db)):
+    """
+    Retrieve a file with messages of a document with fileId
+    """
+    document = dao.dao_document.get(db, id=document_id)
+    if not document:
+        raise HTTPException(status_code=404, detail="Document not found")
+    elif document.status == DocumentStatus.loaded:
+        raise HTTPException(status_code=425, detail="Markup in progress for this document")
+    
+    messages = dao.dao_message.get_by_file_id(db, document_id=document_id)
+    data = {
+        "Дата создания": [],
+        "ID": [],
+        "Текст": [],
+        "Утвержденный блок": [],
+        "Утвержденная тема": [],
+        "Утвержденная локация": [],
+        "Блок": [],
+        "Тема": [],
+        "Локация": []
+    }
+    for msg in messages:
+        data["Дата создания"].append(msg.created_at)
+        data["ID"].append(msg.id)
+        data["Текст"].append(msg.text)
+        data["Утвержденный блок"].append(msg.approved_block)
+        data["Утвержденная тема"].append(msg.approved_theme)
+        data["Утвержденная локация"].append(msg.approved_location)
+        blocks = msg.recognition_blocks
+        block_str = ""
+        for block in blocks:
+            block_probabilty = "%.2f" % round(block.probability, 2)
+            block_str += f"{block.name} ({block_probabilty})%) "
+        data["Блок"].append(block_str)
+        themes = msg.recognition_themes
+        theme_str = ""
+        for theme in themes:
+            theme_probabilty = "%.2f" % round(theme.probability, 2)
+            theme_str += f"{theme.name} ({theme_probabilty})%) "
+        data["Тема"].append(theme_str)
+        locations = msg.recognition_locations
+        location_str = ""
+        for location in locations:
+            location_probabilty = "%.2f" % round(location.probability, 2)
+            location_str += f"{location.street_name} ({location_probabilty})%) "
+
+        data["Локация"].append(location_str)
+
+    df = pandas.DataFrame.from_dict(data)
+    stream = io.StringIO()
+    df.to_csv(stream, index = False)
+    response = StreamingResponse(iter([stream.getvalue()]),
+                                 media_type="text/csv"
+                                )
+    response.headers["Content-Disposition"] = f"attachment; filename={document_id}.csv"
+    return response
 
 @router.patch("/{message_id}/approve/block", response_model=schemas.message.MessageSchema)
 def approve_block(message_id: UUID,
