4193830565114e7f414af6f8f23621aa2b0d55f5
Egor Loktev <loktev@lok-tech.com>
2023-07-03 01:02:43 +0300

    make geo optional

--- dao/message.py
+++ dao/message.py
@@ -55,7 +55,7 @@ class MessageDAO(BaseDAO[Message, MessageCreate, MessageCreate]):
             return \
             db.query(self.model).filter(self.model.document_id == document_id).\
                 join(RecognitionBlock).\
-                filter(RecognitionBlock.id.in_([block]))
+                filter(RecognitionBlock.id.in_([block.id]))
         else:
             return db.query(self.model).filter(self.model.document_id == document_id).all()
 
