0c265d0387bec20e2e1f9f82803aac787ec07d65
Egor Loktev <loktev@lok-tech.com>
2023-07-15 22:47:48 +0300

    fix location schemas

--- schemas/location.py
+++ schemas/location.py
@@ -18,7 +18,7 @@ class Location(BaseModel):
 
 
 class RecognitionLocationBase(BaseModel):
-    probability: float
+    probability: Optional[float}
 
 
 class RecognitionLocationCreate(RecognitionLocationBase):
